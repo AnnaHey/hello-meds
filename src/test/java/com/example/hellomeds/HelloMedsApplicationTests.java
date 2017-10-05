44016528533cb7b5b092c30573affe649a7522f8
AnnaHey <atekton99@gmail.com>
2017-10-05 17:05:41 -0500

    Start with SpringBoot Gradle

---
+++ src/test/java/com/example/hellomeds/HelloMedsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hellomeds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloMedsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
