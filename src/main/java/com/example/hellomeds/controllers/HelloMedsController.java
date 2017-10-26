570a3357d8cfa84602bf7c623ac4cb1570224577
AnnaHey <atekton99@gmail.com>
2017-10-26 16:51:19 -0500

    Work on fragments file

--- src/main/java/com/example/hellomeds/controllers/HelloMedsController.java
+++ src/main/java/com/example/hellomeds/controllers/HelloMedsController.java
@@ -1,16 +1,25 @@
 package com.example.hellomeds.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Anna Hexter on 10/10/2017.
  */
 @Controller
 public class HelloMedsController {
-    @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Hello Med Order App";
+
+    @RequestMapping (value="")
+        public String index(Model model){
+        model.addAttribute("title", "Order My Meds");
+        return "index";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayLoginForm(Model model) {
+        //model.addAttribute("login_name", "username");
+        model.addAttribute("title", "Login");
+        return "login";
     }
 }
