15c134b81f1919423a71969bc02b605448e6067f
AnnaHey <atekton99@gmail.com>
2017-11-07 14:38:46 -0600

    Add private fields

--- src/main/java/com/example/hellomeds/controllers/HelloMedsController.java
+++ src/main/java/com/example/hellomeds/controllers/HelloMedsController.java
@@ -1,8 +1,12 @@
 package com.example.hellomeds.controllers;
+import com.example.hellomeds.models.Patient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 /**
  * Created by Anna Hexter on 10/10/2017.
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloMedsController {
 
-    @RequestMapping (value="")
-        public String index(Model model){
+    static ArrayList<Patient> patients = new ArrayList<>();
+    private String firstName;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
         model.addAttribute("title", "Order My Meds");
         return "index";
     }
@@ -22,4 +29,18 @@ public class HelloMedsController {
         model.addAttribute("title", "Login");
         return "login";
     }
+
+    @RequestMapping(value = "registration", method = RequestMethod.GET)
+    public String displayAddNewPatientForm(Model model) {
+        model.addAttribute("title", "Registration!");
+        return "registration";
+    }
+
+    @RequestMapping(value = "registration", method = RequestMethod.POST)
+    public String addNewPatient(@RequestParam String firstname, @RequestParam String lastname, @RequestParam String username,
+                                @RequestParam String email, @RequestParam String password) {
+        Patient newPatient = new Patient(firstname, lastname, username, email, password);
+        return "registration";
+    }
+
 }
---
+++ src/main/java/com/example/hellomeds/models/Patient.java
@@ -0,0 +1,60 @@
+package com.example.hellomeds.models;
+
+/**
+ * Created by Anna Hexter on 11/2/2017.
+ */
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+    public Patient(String firstName, String lastName, String username, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
